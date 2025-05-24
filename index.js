ce8910acbfe3a5b9d9654f3c8a65639deb0be38e
TaherunNasaToma <taheruntoma@gmail.com>
2025-05-24 21:58:32 +0600

    Added delete operation

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/roommates/:id',async(req,res)=>{
+      const id =req.params.id;
+
+      const query={_id:new ObjectId(id)}
+      const result= await roommatesCollection.deleteOne(query);
+      res.send(result)
+    })
+
 
     
     // Send a ping to confirm a successful connection
