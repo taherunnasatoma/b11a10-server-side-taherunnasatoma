f1aece91d4c8e6b213574c12e98c040f6d47ccea
TaherunNasaToma <taheruntoma@gmail.com>
2025-05-24 15:05:12 +0600

    set get operation for specific id or data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors')
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app= express();
 const port = process.env.PORT || 3000;
@@ -34,6 +34,14 @@ async function run() {
       const cursor = roommatesCollection.find();
       const result=await cursor.toArray()
       res.send(result)
+    })
+
+    app.get('/roommates/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query= {_id:new ObjectId(id)}
+      const result =await roommatesCollection.findOne(query);
+      res.send(result)
+
     })
 
     app.post('/roommates',async(req,res)=>{
